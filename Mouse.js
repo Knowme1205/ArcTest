0121ecabf28b572b891482c403040aaa9ea57cf0
Knowme1205 <sashakossov979@gmail.com>
2026-06-23 20:16:50 +0300

    класс мыши, обрабатывающей события

---
+++ Mouse.js
@@ -0,0 +1,59 @@
+class Mouse{
+    constructor(obj)
+    {
+        this.obj= obj;
+        this.x=0;
+        this.y=0;
+        this.left= false;
+        this.subs = {};
+
+        this.handle (event =>'mousedown ', (e) => this.onMouseDown(e));
+        this.handle (event =>'mouseup ', (e) => this.onMouseUp(e));
+        this.handle (event=>'mousemove ', (e) => this.onMouseMove(e));
+
+    }
+
+    handle(event, callback){
+        this.obj.addEventListener(event, e => {
+            this.onMouseEvent(e,callback);
+            this.trigger(event,e);
+        } , false);
+    }
+
+    on(event, callback)
+    {
+        if(!this.subs[event]) this.subs[event] = [];
+        this.subs[event].push(callback);
+    }
+
+    trigger (event,e)
+    {
+        const callbacks = this.subs[event];
+        callbacks&&callbacks.array.forEach(cb =>cb(e));
+    };
+    
+
+    onMouseUp(e)
+    {
+        this.left = e.button!==0;
+    }
+
+    onMouseDown(e)
+    {
+
+        this.left = e.button===0;
+    }
+    onMouseMove(e)
+    {
+        this.x=e.layerX;
+        this.y=e.layerY;
+    }
+
+    onMouseEvent(e,callback)
+    {
+        callback(e);
+    }
+
+    
+}
+export default Mouse;
